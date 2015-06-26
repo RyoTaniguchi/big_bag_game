6a13c7a679693cbc4de7a6fee4bfb1f06f8e93cf
teramotodaiki <i101323@gmail.com>
2015-06-26 19:38:48 +0900

    Healthをスーパークラスとしてspeedの情報をもたせる

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -3,43 +3,65 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	public GameObject Player;
-	public GameObject Enemy;
+	public GameObject[] Characters;
+	// public GameObject Player;
+	// public GameObject Enemy;
 
 	int n = 2;
-	public int[] x; 
+	// public int[] x;
 
 	// Use this for initialization
 	void Start () {
 		// Kougeki Junban wo Kimeru
-		var hoge = Player.GetComponent<PlayerHealth>();
-		int PlayerSpeed = hoge.speed;
+		// var hoge = Player.GetComponent<PlayerHealth>();
+		// int PlayerSpeed = hoge.speed;
 
-		var hoge1 = Enemy.GetComponent<EnemyHealth>();
-		int EnemySpeed = hoge1.speed;
+		// var hoge1 = Enemy.GetComponent<EnemyHealth>();
+		// int EnemySpeed = hoge1.speed;
 
-		Debug.Log (PlayerSpeed);
-		Debug.Log (EnemySpeed);
+		// Characters から Healthのクラスの情報だけを抜き出した配列を作る
+		var num = Characters.Length;
+		var health = new Health[num];
+		for (var i = 0; i < num; i++)
+		{
+			health[i] = Characters[i].GetComponent<Health>();
+		}
 
+		// Debug.Log (PlayerSpeed);
+		// Debug.Log (EnemySpeed);
+
+		/*
 		x = new int[]{PlayerSpeed,EnemySpeed};
 
 		int i, j, temp;
-		
+
 		for (i = 0; i < n - 1; i++) {
 			for (j = n - 1; j > i; j--) {
-				if (x [j - 1] <= x [j]) {  
-					temp = x [j];       
+				if (x [j - 1] <= x [j]) {
+					temp = x [j];
 					x [j] = x [j - 1];
 					x [j - 1] = temp;
 				}
 			}
 		}
+		*/
+
+		// healthという配列をspeedの順に並べ替える
+		for (var i = 0; i < n - 1; i++) {
+			for (var j = n - 1; j > i; j--) {
+				if (health [j - 1].speed <= health [j].speed) {
+					var temp = health [j];
+					health [j] = health [j - 1];
+					health [j - 1] = temp;
+				}
+			}
+		}
 
 		int a;
-		
+
 		for (a = 0; a < n ; a++)
-			Debug.Log(" "+ x[a]);
-	
+			Debug.Log(" "+ health[a].speed);
+
 	}
 
 }
--- Assets/Script/Health.cs
+++ Assets/Script/Health.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class Health : MonoBehaviour {
 
+	// プレイヤーのスピード
+	public int speed = 4;
+
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
