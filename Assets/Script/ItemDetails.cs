6a444fc141c7c82e904fcb926239f5d11cc8937b
RyoTaniguchi <rx.kino.kinoko@gmail.com>
2015-06-30 07:38:57 +0900

    アイテムが重いと攻撃できないように
    味方のちから(power) < アイテムの重さ（Weaght）だと攻撃できないようにした。

--- Assets/Script/ItemDetails.cs
+++ Assets/Script/ItemDetails.cs
@@ -16,6 +16,18 @@ public class ItemDetails : MonoBehaviour {
 	}
 
 	public void ItemAttack(){
+
+		var hoge3 = GameObject.Find("GM");
+		var hoge4 = hoge3.GetComponent<GameManager> ();
+		var attack =  hoge4.AttackOrder;
+		var order = hoge4.order;
+
+		if (attack [order].power < this.Weight) 
+		{
+			Debug.Log("buki ga omoi!");
+			return;
+		}
+
 		var hoge = GameObject.Find ("Enemy");
 		var hoge1 = hoge.GetComponent<EnemyHealth> ();
 		hoge1.AddjustCurrentHealth (-this.Weight);
